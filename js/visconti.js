44b5aa9ec3b0315a8902bdba2dda65d61771b726
florian <vlo@guez.li>
2012-10-21 19:00:57 +0200

    Probieren wirs mal...

--- js/visconti.js
+++ js/visconti.js
@@ -1,4 +1,4 @@
-var navigation = {
+﻿var navigation = {
 	
 	home: {},
 	uberuns: {},
@@ -7,6 +7,18 @@ var navigation = {
 	demo: {},
 	
 	selected: ko.observable(null),
+	
+	gigs: [
+		{ wann: "15.02.2013", wo: "Luogo lorem Zurigo", link: "http://www.google.com" },
+		{ wann: "18.03.2013", wo: "Luogo lorem Berna", link: "http://www.google.com" },
+		{ wann: "12.04.2013", wo: "Luogo lorem Basilea", link: "http://www.google.com" },
+		{ wann: "15.06.2013", wo: "Luogo lorem Lausanna", link: "http://www.google.com" },
+		{ wann: "19.08.2013", wo: "Luogo lorem Ulan-baator", link: "http://www.google.com" },
+		{ wann: "15.09.2013", wo: "Luogo lorem Moscow", link: "http://www.google.com" },
+		{ wann: "18.09.2013", wo: "Luogo lorem New York", link: "http://www.google.com" },
+		{ wann: "15.02.2013", wo: "Luogo lorem Schützenhaus Unterenstringen-West", link: "http://www.google.com" },
+		{ wann: "12.10.2013", wo: "Luogo lorem Roma", link: "http://www.google.com" }
+	]
 		
 };
 
@@ -14,3 +26,14 @@ $(function () {
 	navigation.selected(navigation.home);
 	ko.applyBindings(navigation, $("#outerContent").get(0));
 });
+
+ko.bindingHandlers.fadeVisible = {
+	init: function(element, valueAccessor) {
+		var value = valueAccessor();
+		$(element).toggle(ko.utils.unwrapObservable(value));
+	},
+	update: function(element, valueAccessor) {
+		var value = valueAccessor();
+		ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).toggle(false);
+	}
+};
